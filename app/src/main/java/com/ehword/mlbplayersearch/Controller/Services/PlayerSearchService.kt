ff3c1dcbb88a9a54fc3efd23a4b6d0dccbfc1145
Anthony <cozziad@gmail.com>
2019-06-02 11:20:10 -0400

    Jun 02 2019 Mid

--- app/src/main/java/com/ehword/mlbplayersearch/Controller/Services/PlayerSearchService.kt
+++ app/src/main/java/com/ehword/mlbplayersearch/Controller/Services/PlayerSearchService.kt
@@ -3,6 +3,7 @@ package com.ehword.mlbplayersearch.Controller.Services
 import android.content.Context
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import com.ehword.mlbplayersearch.Controller.Model.Player
 import com.ehword.mlbplayersearch.Controller.Utilities.URL_PLAYER_SEARCH
 import org.json.JSONException
@@ -11,41 +12,52 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.ehword.mlbplayersearch.Controller.MainActivity
-import com.ehword.mlbplayersearch.R
-import kotlinx.android.synthetic.main.activity_main.playerSearchText
-import kotlinx.android.synthetic.main.activity_main.playerSearchText
+import org.json.JSONObject
 
 
 object PlayerSearchService {
     val players = ArrayList<Player>()
 
-    fun getPlayers(playerId:String, context: Context, complete: (Boolean) -> Unit){
-        //val playerId =  <TextView>(R.id.playerSearchText)
-        val url = URL_PLAYER_SEARCH + playerId
-        println(playerId)
+    fun getPlayers(playerSearchString:String, context: Context, complete: (Boolean) -> Unit){
 
+        val url = URL_PLAYER_SEARCH + playerSearchString +  "%25'"
+        println(url)
         val playersRequest = object : JsonObjectRequest(Method.GET,url ,null, Response.Listener{ response ->
             players.clear()
             try {
-                //println(response.length())
-                //println(response)
-                for (x in 0 until response.length()){
-                  val player = response.getJSONObject("player_info")
-                    val QR = player.getJSONObject("queryResults")
-                    val row = QR.getJSONObject("row")
-                    println(row)
-                       val firstName = row.getString("name_first")
-                       val lastName = row.getString("name_last")
-                       val teamName = row.getString("team_name")
-                       val position = row.getString("primary_position_txt")
-                       val DOB = row.getString("birth_date")
-                       val newPlayer = Player(firstName,lastName,teamName,position,DOB)
-                       this.players.add(newPlayer)
-                   }
+                val responseString = response.getJSONObject("search_player_all")
+                val queryResults = responseString.getJSONObject("queryResults")
+                val playerCount = queryResults.getInt("totalSize")
+                if (playerCount < 1){
+                    Toast.makeText(context,"Could not find any players", Toast.LENGTH_SHORT).show()
+                    return@Listener
+                }
+                if (playerCount == 1){
+                    var curPlayer = queryResults.getJSONObject("row")
+                    val firstName = curPlayer.getString("name_first")
+                    val lastName = curPlayer.getString("name_last")
+                    val teamName = curPlayer.getString("team_full")
+                    val position = curPlayer.getString("position")
+                    val DOB = curPlayer.getString("birth_date")
+                    val newPlayer = Player(firstName, lastName, teamName, position, DOB)
+                    this.players.add(newPlayer)
+                }
+                else {
+                    val playerResults = queryResults.getJSONArray("row")
 
+                    for (x in 0 until playerCount) {
+                        val curPlayer = playerResults.getJSONObject(x)
+                        val firstName = curPlayer.getString("name_first")
+                        val lastName = curPlayer.getString("name_last")
+                        val teamName = curPlayer.getString("team_full")
+                        val position = curPlayer.getString("position")
+                        val DOB = curPlayer.getString("birth_date")
+                        val newPlayer = Player(firstName, lastName, teamName, position, DOB)
+                        this.players.add(newPlayer)
+                        println(players.count())
+                    }
+                }
                 complete (true)
-
             } catch (e: JSONException){
                 Log.d("JSON", "EXC: " + e.localizedMessage)
                 complete (false)
@@ -54,20 +66,8 @@ object PlayerSearchService {
                 error -> Log.d("Error","Could not retrieve players: $error")
             complete (false)
         }) {
-//            override fun getBodyContentType(): String {
-//                return "application/json; charset=utf-8"
-//            }
-//
-//            override fun getHeaders(): MutableMap<String, String> {
-//                val headers = HashMap<String, String>()
-//                headers.put("sport_code", "mlb")
-//                headers.put("active_sw", "Y")
-//                headers.put("name_part", "mill%")
-//                headers.put("search_player_all.col_ex", "player_id")
-//                return headers
-//            }
-        }
 
+        }
 
         val requestQueue = Volley.newRequestQueue(context)
         requestQueue.add(playersRequest)
--- app/src/main/java/com/ehword/mlbplayersearch/Controller/Utilities/Constants.kt
+++ app/src/main/java/com/ehword/mlbplayersearch/Controller/Utilities/Constants.kt
@@ -2,6 +2,7 @@ package com.ehword.mlbplayersearch.Controller.Utilities
 
 const val BASE_URL = "http://lookup-service-prod.mlb.com/json/"
 //const val URL_PLAYER_SEARCH = "${BASE_URL}named.search_player_all.bam?"
-//const val URL_PLAYER_SEARCH ="http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='mill%'&search_player_all.col_ex=player_id"
-const val URL_PLAYER_SEARCH ="http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code='mlb'&player_id="
+//
+const val URL_PLAYER_SEARCH ="http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&search_player_all.col_ex=player_id&name_part='"
+//const val URL_PLAYER_SEARCH ="http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code='mlb'&player_id="
 const val URL_TEAM_SEARCH = "/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw={all_star_sw}&sort_order={sort_order}&season={season}"
